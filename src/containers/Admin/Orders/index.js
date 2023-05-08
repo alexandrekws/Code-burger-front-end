bf660b864440d4bb81b2aa78f9ada547ff791b82
alexandrekws <alexandrekws@uol.com.br>
2023-05-08 15:45:13 +0900

    update server api

--- src/containers/Admin/Orders/index.js
+++ src/containers/Admin/Orders/index.js
@@ -56,6 +56,7 @@ function Orders() {
             )
             setfilteredOrders(newFilteredOrders)
         }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [orders])
 
     function handleStatus(status) {
